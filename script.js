de100bf5bfc596a86133ca650465037d888171eb
l2chadraoui <lbc04@mail.aub.edu>
2025-01-28 19:51:53 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+function toggleDescription(id) {
+    let element = document.getElementById(id);
+
+    // Close all open descriptions
+    document.querySelectorAll('.description').forEach(desc => {
+        if (desc.id !== id) {
+            desc.classList.remove('active');
+        }
+    });
+
+    // Toggle selected description
+    element.classList.toggle('active');
+}
